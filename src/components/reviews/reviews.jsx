d460ef3f650956aca1a7451d38d2a9d56f619401
aaqilruzzan <aaqil.ruzzan@gmail.com>
2024-03-17 11:38:03 +0530

    changes for mobile responsiveness and added a review

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -1,7 +1,7 @@
 export const Reviews = () => {
 	return (
 		<>
-			<div class="grid gap-6 text-center md:grid-cols-1 lg:gap-12">
+			<div class="mt-16 grid gap-6 text-center md:grid-cols-2 md:mt-0 lg:gap-12 ">
 				<div class="mb-12 md:mb-0">
 					<div class="mb-6 flex justify-center">
 						<img
@@ -34,6 +34,45 @@ export const Reviews = () => {
 						make him a valuable asset to any team. I highly
 						recommend Ahamed for any future endeavors in software
 						engineering
+					</p>
+				</div>
+				<div class="mb-12 md:mb-0">
+					<div class="mb-6 flex justify-center">
+						<img
+							src="../amjad.jpg"
+							alt="Minhaz Mohamed"
+							class="w-32 rounded-full shadow-lg dark:shadow-black/30"
+						/>
+					</div>
+					<h5 class="mb-4 text-xl font-semibold">Amjad Nazar</h5>
+					<h6 class="mb-4 font-semibold text-primary dark:text-primary-500">
+						Software Engineer at Corzent
+					</h6>
+					<p class="mb-4">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							class="inline-block h-7 w-7 pr-2"
+							viewBox="0 0 24 24"
+						>
+							<path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z" />
+						</svg>
+						I have the pleasure of currently managing Aaqil's work
+						at Win Authority, and I must say he continues to shine
+						with his exceptional contributions to our ongoing
+						projects. Aaqil has consistently proven himself to be a
+						fast learner, surpassing expectations with his ability
+						to grasp new concepts. His problem-solving skills are
+						truly remarkable, particularly when compared to others
+						at his level. What sets Aaqil apart is not just his
+						technical skills, but also his attentiveness as a
+						listener and his proactive approach to incorporating
+						feedback. He takes constructive criticism in stride,
+						promptly addressing any areas for improvement and
+						consistently striving for excellence in his work.
+						Aaqil's dedication and talent make him a valuable asset
+						to our team, and I have no doubt he will continue to
+						excel in his future endeavors.
 					</p>
 				</div>
 			</div>
